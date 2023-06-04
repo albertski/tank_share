237930d29464ce6a21e916d11f0a6f66556d1ddf
Albert Jankowski <ajankowski@q-centrix.com>
2023-06-04 12:25:40 -0500

    Updates signup form

--- app/controllers/email_signup_controller.rb
+++ app/controllers/email_signup_controller.rb
@@ -7,18 +7,39 @@ class EmailSignupController < ApplicationController
 
   def create
     @email_signup = EmailSignup.new(email_signup_params)
-    partial = 'email_signup/message'
     if @email_signup.save
-      flash.now[:notice] = 'Thank you for signing up!'
+      flash.now[:notice] = "Thank you #{first_name} for signing up!"
     else
       flash.now[:alert] = 'An error occurred. Please try again.'
     end
-    render turbo_stream: turbo_stream.replace('message', partial)
+    render turbostream
   end
 
   private
 
   def email_signup_params
-    params.permit(:first_name, :last_name, :email)
+    params.require(:email_signup).permit(:first_name, :last_name, :email)
+  end
+
+  def first_name
+    @email_signup.first_name
+  end
+
+  def message_partial
+    render_to_string(partial: 'email_signup/message')
+  end
+
+  def form_partial
+    render_to_string(partial: 'email_signup/form',
+                     locals: { email_signup: @email_signup })
+  end
+
+  def turbostream
+    {
+      turbo_stream: [
+        turbo_stream.replace('email_signup_message', message_partial),
+        turbo_stream.replace('email_signup_form', form_partial)
+      ]
+    }
   end
 end
--- spec/system/signup_spec.rb
+++ spec/system/signup_spec.rb
@@ -13,13 +13,22 @@ RSpec.describe 'Signup', type: :system do
         fill_in 'Email', with: 'john@example.com'
       end
 
-      context 'and email does not exist already' do
+      context 'and email does not exist' do
         it do
           expect do
             click_on 'Sign Up'
-            text = 'Thank you for signing up!'
-            expect(page).to have_selector('#message', text)
+            expect(page).to have_content('Thank you John for signing up!')
           end.to change(EmailSignup, :count).by(1)
+        end
+      end
+
+      context 'and email already exists' do
+        let!(:email_signup) { create(:email_signup, email: 'john@example.com') }
+        it do
+          expect do
+            click_on 'Sign Up'
+            expect(page).to have_content('An error occurred. Please try again.')
+          end.to change(EmailSignup, :count).by(0)
         end
       end
     end
