f47d57d36d01ee033b7ca8fc84a4133fb899fd3d
Albert Jankowski <ajankowski@q-centrix.com>
2023-09-27 22:57:36 -0500

    Add /up route

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'signup', to: 'email_signup#show'
   post 'signup', to: 'email_signup#create'
+  get 'up', to: proc { [200, {}, ['OK']] }
 end
