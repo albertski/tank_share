4b2929222a30e5405ab0c2b68bcf89b3038e56f2
Albert Jankowski <ajankowski@q-centrix.com>
2024-01-14 16:09:48 -0600

    Removes invalid character

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
-- config.action_mailer.default_url_options = { host: 'https://www.tankshare.net' }
+  config.action_mailer.default_url_options = { host: 'https://www.tankshare.net' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
