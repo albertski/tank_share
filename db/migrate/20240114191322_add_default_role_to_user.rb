f6bc1cfb6a2f371ec66c3c547a535fe44c97cdec
Albert Jankowski <ajankowski@q-centrix.com>
2024-01-14 13:20:56 -0600

    Add member as a defalt role for new users

---
+++ db/migrate/20240114191322_add_default_role_to_user.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class AddDefaultRoleToUser < ActiveRecord::Migration[7.1]
+  def change
+    change_column_default :users, :role, from: nil, to: 0
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_15_233834) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_14_191322) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,7 +40,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_15_233834) do
     t.text "country"
     t.integer "years_tanking"
     t.integer "level"
-    t.integer "role"
+    t.integer "role", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
