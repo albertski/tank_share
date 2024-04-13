5ca02c0e0f111bdf9b23f920f3ebd7006288cfb0
Albert Jankowski <albertskiski@gmail.com>
2024-04-13 10:16:35 -0500

    Remove old fields

--- app/models/tank.rb
+++ app/models/tank.rb
@@ -2,10 +2,6 @@
 
 class Tank < ApplicationRecord
   has_many_attached :images
-  # measured_volume :total_volume_value, unit_field_name: :total_volume_unit
-  # measured_volume :display_volume_value, unit_field_name: :display_volume_unit
-  # measured_volume :sub_volume_value, unit_field_name: :sub_volume_unit
-  # measured_length :glass_thickness, unit_field_name: :glass_thickness_unit
 
   measured Measured::Volume, :total_volume
   measured Measured::Volume, :display_volume
